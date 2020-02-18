89192c11b417d98d6f188524d3fd2999363c3c90
shibashiba48 <t1257476@gmail.com>
2020-02-18 20:59:09 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'chatspace'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:shibashiba48/chat-space.git'
